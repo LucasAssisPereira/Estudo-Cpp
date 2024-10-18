92e8ab7d7d3a7431c9c9cf82a29d6b480582000e
Lucas Assis Pereira <lucasassis1905@gmail.com>
2024-10-18 07:43:22 -0300

    Operadores Matematicos

---
+++ OperadoresMatematicos/OperadoresMatematicos.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float num1, num2, divisao;
+	
+	
+	
+	
+	cout << "Digite o primeiro numero: ";
+	cin >> num1;
+	
+	cout << "Digite o segundo numero: ";
+	cin >> num2;
+	
+	divisao = num1 / num2;
+	
+	cout << "Soma: " << num1 + num2 << "\n";
+	cout << "Subtracao: " << num1 - num2 << "\n";
+	cout << "Multiplicao: " << num1 * num2 << "\n";
+	cout << "Divisao: " << divisao << "\n";
+	 
+	
+	
+	return 0;
+}
