32c3e8fd3fd44fba22a586a15243a9bd73386abc
Lucas Assis Pereira <lucasassis1905@gmail.com>
2024-10-18 00:22:21 -0300

    Segundo exercicio

---
+++ InputAndPrint/InputAndPrint.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std ;
+
+int main(){
+	
+	int inteiro = 0;
+	float flutuante = 0;
+	
+	cout << "Numero Inteiro: ";
+	cin >> inteiro;
+	cout << "Your number is: " << inteiro << "\n";
+	
+	cout << "Numero Float: ";
+	cin >> flutuante ;
+	cout << "Your number is: " << flutuante << "\n";
+	
+	
+
+	
+	return 0;
+}
